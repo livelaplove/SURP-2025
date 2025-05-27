ed567151beb53bdf0101f63830793ed517bfe310
livelaplove <136587288+livelaplove@users.noreply.github.com>
2025-05-27 13:37:38 -0400

    made code look better

---
+++ metallicity/datamanip.py
@@ -0,0 +1,150 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import statistics as stat
+
+
+def writecsv(csv1, csv2, id1, id2, destination):
+    '''
+    Merges csvs to produce dataframe.
+    '''
+
+    df1 = pd.read_csv(csv1)
+    df2 = pd.read_csv(csv2)
+    df1 = df1.loc[df1[id1].isin(df2[id2])]
+
+    df = pd.merge(df1, df2, left_on=id1, right_on=id2, how='inner')
+    df.to_csv(destination)
+
+    return
+
+def findunique(csv1, csv2, id1, id2, destination):
+    '''
+    Compares two csvs and removes common rows based on value of columns
+    '''
+
+    df1 = pd.read_csv(csv1)
+    df2 = pd.read_csv(csv2)
+
+    df = df1.loc[~df1[id1].isin(df2[id2])]
+    df.to_csv(destination)
+
+    return df
+
+def readcsv(csv, x = None, xbin = None, xbinlabel = None, hue = None, huebin = None, huebinlabel = None):
+    '''
+    Reads csv with option for creating bins and subbins based on given intervals
+    '''
+
+    df = pd.read_csv(csv)
+
+    if x == None:
+        if hue != None:
+            huebin = pd.cut(df[hue], huebin, labels=huebinlabel, include_lowest=True)
+
+            df = df.drop(hue, axis=1)
+            df = df.merge(huebin, how='inner', left_index=True, right_index=True)
+        else:
+            pass
+
+        return df
+        
+    else: 
+        xbins = pd.cut(df[x], xbin, labels=xbinlabel, include_lowest=True)
+        huebins = pd.cut(df[hue], huebin, labels=huebinlabel, include_lowest=True)
+
+        df = df.drop([x, hue], axis=1)
+        df = df.merge(xbins, how='inner', left_index=True, right_index=True)
+        df = df.merge(huebins, how='inner', left_index=True, right_index=True)
+
+        return df
+
+def readdf(df, x, hue, xbin, xbinlabel, huebin, huebinlabel):
+    '''
+    Pandas dataframe version of readcsv.
+    '''
+
+    xbins = pd.cut(df[x], xbin, labels=xbinlabel, include_lowest=True)
+    huebins = pd.cut(df[hue], huebin, labels=huebinlabel, include_lowest=True)
+
+    df = df.drop([x, hue], axis=1)
+    df = df.merge(xbins, how='inner', left_index=True, right_index=True)
+    df = df.merge(huebins, how='inner', left_index=True, right_index=True)
+
+    return df
+
+def bprp_to_teff(bprp):
+    """
+    Calculate photometric Teff from Gaia color (use dereddened color!)
+    Args:
+        bprp (array): Gaia G_BP colour minus Gaia G_RP colour.
+    Returns:
+        teffs (array): Photometric effective temperatures.
+    """
+
+    coeffs = [8959.8112335205078, -4801.5566310882568, 1931.4756631851196,
+            -2445.9980716705322, 2669.0248055458069, -1324.0671020746231,
+            301.13205924630165, -25.923997443169355]
+    """
+    # Jason's updated parameters:
+    coeffs = [-416.585, 39780.0, -84190.5, 85203.9, -48225.9, 15598.5,
+              -2694.76, 192.865]
+    """
+
+    return np.polyval(coeffs[::-1], bprp)
+
+def addteff(csv):
+    '''
+    Converts bp-rp to teff.
+    '''
+
+    df = pd.read_csv(csv)
+    teff = pd.Series(bprp_to_teff(df['bp_rp']), name='teff')
+    df = pd.merge(df, teff, left_index=True, right_index=True, how='inner')
+    df.to_csv(csv)
+
+    return df
+
+def findbincounts(df, bin, subbin=None):
+    if subbin != None:
+        return df.groupby(by=[subbin, bin]).size().reset_index(name='counts')
+    else:
+        return df.groupby(by=bin).size().reset_index(name='counts')
+
+def calcvzdisp(df, mhbinlabels, x=None, xbinlabels=None):
+    '''
+    Finds sample standard deviation of vertical velocity dispersion based on metallicity and/or stellar property bin.
+    '''
+
+    mhbins = []
+    samplesizes = []
+    vzdisps = []
+
+    if xbinlabels == None:
+        for mhbin in mhbinlabels:
+            sample = df.loc[df['mh_xgboost'] == mhbin]
+            vzdisp = round(stat.stdev(sample['vz']), 3)
+
+            mhbins.append(mhbin)
+            samplesizes.append(len(sample))
+            vzdisps.append(vzdisp)
+
+        return pd.DataFrame({'mh_xgboost':mhbins, 'samplesize':samplesizes, 'vzdisp':vzdisps})
+            
+    
+    else:
+        xbins = []
+        for xbin in xbinlabels:
+            for mhbin in mhbinlabels:
+
+                sample = df.loc[df['mh_xgboost'] == mhbin]
+                sample = sample.loc[df[x] == xbin]
+                vzdisp = round(stat.stdev(sample['vz']), 3)
+
+                xbins.append(xbin)
+                mhbins.append(mhbin)
+                samplesizes.append(len(sample))
+                vzdisps.append(vzdisp)
+
+        return pd.DataFrame({x:xbins, 'mh_xgboost':mhbins, 'samplesize':samplesizes, 'vzdisp':vzdisps})
+    
--- metallicity/plotting.py
+++ metallicity/plotting.py
@@ -6,6 +6,47 @@ from matplotlib import patches
 
 px = 1/plt.rcParams['figure.dpi']
 
+def plotparams():
+    '''
+    Holds plot parameters for legibility.
+    '''
+
+    plt.rcParams.keys()
+    plt.rc('font', family='serif')
+    params = {
+    'axes.labelsize': 30,
+    'axes.linewidth': 1.5,
+    'legend.fontsize': 25,
+    'legend.frameon': False,
+    'lines.linewidth': 2,
+    'xtick.direction': 'in',
+    'xtick.labelsize': 20,
+    'xtick.major.bottom': True,
+    'xtick.major.pad': 10,
+    'xtick.major.size': 10,
+    'xtick.major.width': 1,
+    'xtick.minor.bottom': True,
+    'xtick.minor.pad': 3.5,
+    'xtick.minor.size': 5,
+    'xtick.minor.top': True,
+    'xtick.minor.visible': True,
+    'xtick.minor.width': 1,
+    'xtick.top': True,
+    'ytick.direction': 'in',
+    'ytick.labelsize': 20,
+    'ytick.major.pad': 10,
+    'ytick.major.size': 10,
+    'ytick.major.width': 1,
+    'ytick.minor.pad': 3.5,
+    'ytick.minor.size': 5,
+    'ytick.minor.visible': True,
+    'ytick.minor.width': 1,
+    'ytick.right': True,
+    'figure.figsize': [10,10], # instead of 4.5, 4.5
+    'savefig.format': 'eps',
+    }
+    plt.rcParams.update(params)
+
 def boxplotdf(df, x, y, hue, xorder, hueorder, palette, title, show=True, figsize=(800*px,1600*px), notch=False):
     '''
     Creates boxplot.
@@ -48,3 +89,16 @@ def addhistcounts(ax, bincounts, size='small', color='w'):
         ax.text(box.get_x() + box.get_width() / 4 ,box.get_y() + 3 * box.get_height() / 4, count)
 
     return None
+
+def scatterplot(df, x, y, hue, hueorder, title, palette, show=False, figsize=(10,5)):
+    plt.figure(figsize=figsize)
+    ax = sns.scatterplot(data=df, x=x, y=y, hue=hue, hue_order=hueorder, size=3, palette=palette, alpha=0.5)
+    ax.set_title(title, fontsize=18)
+
+    if show == True:
+        plt.show()
+
+    return ax
+
+def manyscatterplots():
+    pass
